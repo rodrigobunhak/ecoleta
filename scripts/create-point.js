700e205498350e4a442da0f7ef9a747e83d328bc
Rodrigo Augusto Bunhak Zwierzikowski <rodrigo.bunhak@gmail.com>
2020-06-02 17:20:37 -0300

    corrigido bug que acumula as cidades

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,14 +1,18 @@
+// Função para ordenar os objetos pelo atributo nome
 function ordenarPorNome(a, b){
     return a.nome < b.nome ? -1 : a.nome > b.nome ? 1 : 0;
   }
 
+// função que faz requisição api no cadastro de ufs e popula o campo de select
 function populateUfs() {
   const ufSelect = document.querySelector("select[name=uf]");
 
+  // Promisse responsável por solicitar e tratar dados da api
   fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
   .then( res => res.json() )
   .then( states => {
     for( const state of states.sort(ordenarPorNome)) {
+      // escreve no document os dados
       ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
     }
   })
@@ -26,6 +30,8 @@ function getCities(){
   stateInput.value = event.target.options[indexOfSelectedState].text;
 
   const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
+
+  citySelect.innerHTML = "";
 
   fetch(url)
   .then( res => res.json() )
