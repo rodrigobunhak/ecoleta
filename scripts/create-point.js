e401e81e313f9b50f3087b796aa410eaa85d97dd
Rodrigo Augusto Bunhak Zwierzikowski <rodrigo.bunhak@gmail.com>
2020-06-03 00:29:24 -0300

    ajustada a chamada da promisse

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,21 +1,24 @@
 // Função para ordenar os objetos pelo atributo nome
 function ordenarPorNome(a, b){
-    return a.nome < b.nome ? -1 : a.nome > b.nome ? 1 : 0;
-  }
+  return a.nome < b.nome ? -1 : a.nome > b.nome ? 1 : 0;
+}
+
+function minhaPromisse(url, campo) {
+  fetch(url)
+  .then( res => res.json() )
+  .then( objects => {
+    for( const object of objects.sort(ordenarPorNome)) {
+      // escreve no document os dados
+      campo.innerHTML += `<option value="${object.id}">${object.nome}</option>`
+    }
+  })
+}
 
 // função que faz requisição api no cadastro de ufs e popula o campo de select
 function populateUfs() {
   const ufSelect = document.querySelector("select[name=uf]");
 
-  // Promisse responsável por solicitar e tratar dados da api
-  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-  .then( res => res.json() )
-  .then( states => {
-    for( const state of states.sort(ordenarPorNome)) {
-      // escreve no document os dados
-      ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
-    }
-  })
+  minhaPromisse("https://servicodados.ibge.gov.br/api/v1/localidades/estados", ufSelect);
 }
 
 populateUfs();
@@ -33,17 +36,10 @@ function getCities(){
 
   citySelect.innerHTML = "";
 
-  fetch(url)
-  .then( res => res.json() )
-  .then( cities => {
-    for( const city of cities.sort(ordenarPorNome)) {
-      citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
-    }
-  })
+  minhaPromisse(url, citySelect);
+
   citySelect.disabled = false;
 }
-
-
 
 document
   .querySelector("select[name=uf]")
