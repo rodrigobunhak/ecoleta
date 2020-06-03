f22c533dc05708e8a3ded573973247d99538935c
Rodrigo Augusto Bunhak Zwierzikowski <rodrigo.bunhak@gmail.com>
2020-06-03 15:33:39 -0300

    finalizado o css do modal na tela inicial

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,46 +1,91 @@
-// Função para ordenar os objetos pelo atributo nome
+// ordena os objetos pelo atributo nome
 function ordenarPorNome(a, b){
   return a.nome < b.nome ? -1 : a.nome > b.nome ? 1 : 0;
 }
 
-function minhaPromisse(url, campo) {
+// faz a requisição na api e constroi os elementos options
+function createOption(url, campo) {
   fetch(url)
   .then( res => res.json() )
   .then( objects => {
     for( const object of objects.sort(ordenarPorNome)) {
-      // escreve no document os dados
       campo.innerHTML += `<option value="${object.id}">${object.nome}</option>`
     }
   })
 }
 
-// função que faz requisição api no cadastro de ufs e popula o campo de select
 function populateUfs() {
   const ufSelect = document.querySelector("select[name=uf]");
-
-  minhaPromisse("https://servicodados.ibge.gov.br/api/v1/localidades/estados", ufSelect);
+  createOption("https://servicodados.ibge.gov.br/api/v1/localidades/estados", ufSelect);
 }
-
 populateUfs();
 
-function getCities(){
+function populateCities(){
   const citySelect = document.querySelector("select[name=city]");
   const stateInput = document.querySelector("input[name=state]");
-
-  const ufValue = event.target.value;
-
+  const cityInput = document.querySelector("input[name=city]")
+  const ufValue = event.target.value; // pega o id do municipio
+  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
+  
+  // pega o nome do estado selecionado e coloca no elemento input para ser enviado pelo formulárop
   const indexOfSelectedState = event.target.selectedIndex;
   stateInput.value = event.target.options[indexOfSelectedState].text;
 
-  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
+  // pega o nome da cidade selecionada e coloca no elemento input para ser enviado pelo formulario
+  citySelect.addEventListener("change", function(){
+    const indexOfSelectedCity = event.target.selectedIndex;
+    cityInput.value = event.target.options[indexOfSelectedCity].text;
+  })
 
-  citySelect.innerHTML = "";
+  // reseta o valor do elemento select
+  citySelect.innerHTML = `<option value="">Selectione a Cidade</option>`;
+  citySelect.disabled = true;
 
-  minhaPromisse(url, citySelect);
+  // chama api e popula elemento select
+  createOption(url, citySelect);
 
+  // habilita o campo para seleção
   citySelect.disabled = false;
 }
 
+// adiciona evento change ao campo seleção de estado
 document
   .querySelector("select[name=uf]")
-  .addEventListener("change", getCities)
+  .addEventListener("change", populateCities)
+
+
+const itemsToCollected = document.querySelectorAll(".items-grid li")
+const collectedItems = document.querySelector("input[name=items]")
+
+// adiciona evento de clique nos itens da lista
+for (const item of itemsToCollected) {
+  item.addEventListener("click", handleSelectedItem)
+}
+
+// array vazio que vai ser preenchido com os itens selecionados
+let selectedItems = []
+
+function handleSelectedItem(event) {
+  const itemLi = event.target
+  const itemId = itemLi.dataset.id
+
+  // adiciona/remove classe do elemento
+  itemLi.classList.toggle("selected")
+
+  // verifica se existem itens selecionados, se sim
+  // pega os itens selecionados
+  // retona o index onde foi encontrado o valor e caso nao encontre retornar -1
+  const alreadySelected = selectedItems.findIndex( item => item == itemId )
+  
+  // se já tiver selecionado, tira da selecao
+  if (alreadySelected >= 0){
+    const filteredItems = selectedItems.filter( item => {
+      const itemIsDifferent = item != itemId
+      return itemIsDifferent
+    })
+    selectedItems = filteredItems
+  } else {
+    selectedItems.push(itemId)
+  } 
+  collectedItems.value = selectedItems  
+}
