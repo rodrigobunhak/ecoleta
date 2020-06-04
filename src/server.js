4e90c9f1a8f3929132b25f4d92edf2caa3643d3a
Rodrigo Augusto Bunhak Zwierzikowski <rodrigo.bunhak@gmail.com>
2020-06-04 11:12:14 -0300

    instalado e configurado nunjucks

--- src/server.js
+++ src/server.js
@@ -3,16 +3,22 @@ const server = express()
 
 server.use(express.static("public"))
 
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+	express: server,
+	noCache: true
+})
+
 server.get("/", (reg, res) => {
-	res.sendFile(__dirname + "/views/index.html")
+	res.render("index.html")
 })
 
 server.get("/create-point", (reg, res) => {
-	res.sendFile(__dirname + "/views/create-point.html")
+	res.rende("create-point.html")
 })
 
 server.get("/search-results", (reg, res) => {
-	res.sendFile(__dirname + "/views/search-results.html")
+	res.render("search-results.html")
 })
 
 server.listen(3000)
